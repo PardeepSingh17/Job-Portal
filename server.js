244aefb58d99132be4402d68e3270dde18b079c7
Pardeep Singh <pard6508@gmail.com>
2026-05-11 09:51:44 +0530

    Fixed frontend problem with cors

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const cors = require("cors")
 const app = express()
 
 app.use(cors({
-  origin: "http://localhost:5173"
+    origin: [
+        "http://localhost:5173",
+        "https://job-portal-frontend-coral-pi.vercel.app"
+    ],
+    credentials: true
 }))
 
 const morgan = require("morgan")
